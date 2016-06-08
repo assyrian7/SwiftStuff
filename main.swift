46e959b9c6d3ee28a7c01984bf55513533a0ab9c
Paul Badalian <paulbadalian@Pauls-Mac.local>
2016-06-08 13:37:17 -0700

    First Commit

---
+++ main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  SwiftPrac
+//
+//  Created by Paul Badalian on 6/8/16.
+//  Copyright © 2016 Paul Badalian. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+func addTwo(one: Int, two: Int) -> Int{
+    return one + two
+}
+print(addTwo(3, two: 4))
+
